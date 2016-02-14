3d70d46cb2481b3fee5191eea8dceb0f1fe8bb17
czarandy <andy.goder@gmail.com>
2016-02-13 22:57:02 -0800

    hash routing

--- src/js/app.react.js
+++ src/js/app.react.js
@@ -308,6 +308,6 @@ var routes = (
   </Route>
 );
 
-Router.run(routes, Router.HistoryLocation, function(Handler) {
+Router.run(routes, function(Handler) {
   React.render(<Handler />, document.getElementById('approot'));
 });
