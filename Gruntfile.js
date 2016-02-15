6511a02f963264af248d7d1d0d28c986b8c017f3
czarandy <andy.goder@gmail.com>
2016-02-14 23:15:14 -0800

    typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,7 +101,7 @@ module.exports = function(grunt) {
           'build/js/app.js' : ['build/js/app.js'],
           'build/data/players.js' : ['build/data/players.js'],
           'build/data/recent.js' : ['build/data/recent.js'],
-          'build/data/tournament.js' : ['build/data/tournament.js']
+          'build/data/tournaments.js' : ['build/data/tournaments.js']
         }
       }
     },
