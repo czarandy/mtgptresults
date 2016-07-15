26c36f4613cee7e5c43a7d0665071bad2909da04
Andrey Goder <andy.goder@gmail.com>
2016-07-15 15:23:36 -0700

    update connect to redirect urls appropriately

--- grunt/connect.js
+++ grunt/connect.js
@@ -6,7 +6,21 @@ module.exports = {
       base: 'build',
       port: 8000,
       open: true,
-      keepalive: true
+      keepalive: true,
+      middleware: function(connect, options, middleware) {
+        middleware.unshift(
+          function(req, res, next) {
+            if (req.url.endsWith('js')
+                || req.url.endsWith('css')
+                || req.url.endsWith('svg')
+                || req.url.endsWith('png')) {
+              return next();
+            }
+            require('fs').createReadStream('build/index.html').pipe(res);
+          }
+        );
+        return middleware;
+      }
     }
   }
 };
